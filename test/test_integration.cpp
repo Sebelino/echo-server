ed002175739930cedde6e5726fb76638a025edfc
Sebastian Olsson <sebelino7@gmail.com>
2016-07-04 09:34:51 +0200

    Removed rudimentary dummy test cases.

--- test/test_integration.cpp
+++ test/test_integration.cpp
@@ -6,17 +6,9 @@
     #define SERVER_H
 #include "../src/server.hpp"
 
-TEST(SquareRootTest, PositiveNos) {
-    EXPECT_EQ(18.0, square_root(324));
-    EXPECT_EQ(25.4, square_root(645.16));
-    EXPECT_EQ(4, square_root(16));
-}
 
-TEST(SquareRootTest, ZeroAndNegativeNos) {
-    ASSERT_EQ(0.0, square_root(0.0));
-    EXPECT_ANY_THROW({
-        square_root(-22.0);
-    });
+TEST(EchoTest, HelloWorld) {
+    // Unimplemented
 }
 
 int main(int argc, char **argv) {
